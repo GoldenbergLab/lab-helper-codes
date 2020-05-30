4527f853bb47e8a00591aee6e40d4c665dfa4996
Jschoene <44033635+Jschoene@users.noreply.github.com>
2020-05-30 19:41:26 +0100

    readme general

--- image_manipulation/Picture_Formatter.py
+++ image_manipulation/Picture_Formatter.py
@@ -6,6 +6,19 @@ import os
 import numpy as np
 ### READ ME
 
+#Generally
+# This code has 4 main functions which can be applied to a single image or a whole directory of images
+# name changer: takes a picture and changes the number index of the picture according to the rule, which is right now, if a picture is for e.g. A01 it will add 50 to it to make it A51.
+# you can change the roles for labeling according to your needs
+# size changer: takes an image and changes the size proportionally. Right now it creates images of the widht 141pixels
+# background_color_changer: takes a picture and changes the background to grey
+# remover: removes files from directory
+# LOOPS AT THE BOTTOM: They can be used to process a whole batch/directory
+# LOOP 1: changes all names by creating a copy with the new name. It also removes the old version with the old name
+# LOOP 2: changes the background and the size of all files
+
+
+# For tasks related to image Morphs
 # This code transforms the original morphs that you get from fantamorph into the right SIZE, LABEL, and BACKGROUND
 # In case that some pictures are to bright change the variables such as sensitiyity in the function background_color_changer
 # Mind that the input picture size has to be around 500 * 650
@@ -27,7 +40,7 @@ def name_changer(file):
 
 
 def size_changer(file):
-	basewidth = 141
+	basewidth = 141 # in pixel
 	img = Image.open(file)
 	wpercent = (basewidth / float(img.size[0]))
 	hsize = int((float(img.size[1]) * float(wpercent)))
@@ -68,16 +81,16 @@ def remover (files):
 	if number_of_image < 51:
 		os.remove(file)
 
-for file in os.listdir(file_location):
+# LOOP 1
+for file in os.listdir(file_location): #This loop changes all names by creating a copy with the new name. It also removes the old version with the old name
 	if file.endswith(".jpg"):
 		name = name_changer(file)
 		ImageOps.expand(Image.open(file)).save(name)
 		remover(file)
 
-for file in os.listdir(file_location):
+# LOOP 2
+for file in os.listdir(file_location): #this loop changes the background and the size of all files
 	if file.endswith(".jpg"):
-		name = name_changer(file)
-		remover(file)
 		background_color_changer(file)
 		size_changer(file)
 
