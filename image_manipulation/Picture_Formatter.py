02f8ca2b4aa9ecc1c59b744e329c9577d682d9a9
Jschoene <44033635+Jschoene@users.noreply.github.com>
2020-05-30 19:20:40 +0100

    README

--- image_manipulation/Picture_Formatter.py
+++ image_manipulation/Picture_Formatter.py
@@ -4,7 +4,13 @@ import re
 import cv2
 import os
 import numpy as np
+### READ ME
 
+# This code transforms the original morphs that you get from fantamorph into the right SIZE, LABEL, and BACKGROUND
+# In case that some pictures are to bright change the variables such as sensitiyity in the function background_color_changer
+# Mind that the input picture size has to be around 500 * 650
+# otherwise you have to adjust the x, y, w, h = 140, 100, 200, 550 values in background_color_changer
+# I will change that requirement at some point :D
 
 # listing all files
 current_path = os.getcwd()
