b15b537e8670be1d3dbeb703b070936ec4e52dcd
Jschoene <44033635+Jschoene@users.noreply.github.com>
2020-06-15 15:04:57 +0100

    LIWC_For_Python

---
+++ sentiment_analysis/LIWC_analysis.py
@@ -0,0 +1,25 @@
+import re
+from collections import Counter
+import liwc
+
+def tokenize(text):
+    # you may want to use a smarter tokenizer
+    for match in re.finditer(r'\w+', text, re.UNICODE):
+        yield match.group(0)
+
+
+parse, category_names = liwc.load_token_parser('D:\\Desktop\\Social_Sharing\\raw_data\\e2015.dic')
+
+gettysburg = '''Four score and seven years ago our fathers brought forth on
+  this continent a new nation, conceived in liberty, and dedicated to the
+  proposition that all men are created equal. Now we are engaged in a great
+  civil war, testing whether that nation, or any nation so conceived and so
+  dedicated, can long endure. We are met on a great battlefield of that war.
+  We have come to dedicate a portion of that field, as a final resting place
+  for those who here gave their lives that that nation might live. It is
+  altogether fitting and proper that we should do this.'''
+gettysburg_tokens = tokenize(gettysburg)
+# now flatmap over all the categories in all of the tokens using a generator:
+gettysburg_counts = Counter(category for token in gettysburg_tokens for category in parse(token))
+# and print the results:
+print(gettysburg_counts)
---
+++ sentiment_analysis/encrypting_liwc_dic.py
@@ -0,0 +1,34 @@
+from itertools import cycle
+import base64
+import zipfile
+import re
+import sys
+"""
+Example command:
+python script.py "./original/LIWC2015_English.dic" "./e2015.dic" "/home/user/.wine/drive_c/Program Files (x86)/LIWC2015/app/lib/LIWC2015-app-1.5.0.jar"
+"""
+
+input_dic = 'D:\\Desktop\\Social_Sharing\\raw_data\\LIWC2015_English_Flat.dic'
+output_dic = 'D:\\Desktop\\Social_Sharing\\raw_data\\e2015.dic'
+
+jar_location = 'D:\\Software\\LIWC2015\\app\\lib\\LIWC2015-app-1.6.0.jar'
+class_name = 'com/liwc/LIWC2015/model/Dictionary.class'
+expr = re.compile(b'\x0e([\x20-\x7f]+)\x07')
+key = None
+
+with zipfile.ZipFile(jar_location, 'r') as zf:
+    with zf.open(class_name, 'r') as byte_stream:
+        byte_code = byte_stream.read()
+        all_matches = re.findall(expr, byte_code)
+        if len(all_matches) >= 1:
+            key = str.encode(all_matches[0].decode())
+
+if key is None:
+    print("Key not found. Can't proceed.")
+else:
+    with open(input_dic, 'rb') as f:
+        message = f.read()
+
+    decrypted_msg = ''.join([chr(i^j) for i, j in zip(base64.decodebytes(message), cycle(key))])
+    with open(output_dic, 'wb') as f:
+        f.write(str.encode(decrypted_msg))
